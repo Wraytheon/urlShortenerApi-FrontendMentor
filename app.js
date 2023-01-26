8a63a5613b8fe88bf4f6a7e7557dd065857dfbcf
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2023-01-25 21:32:29 -0500

    placeholder nav complete, main img in place

---
+++ app.js
@@ -0,0 +1,19 @@
+// //Hamburger Menu
+// const burgerIcon = document.querySelector(".fa-bars");
+// const navMenu = document.querySelector(".menu-section");
+// const mainElement = document.querySelector(".main-container");
+
+// //Toggle Menu
+// const menuToggle = () => navMenu.classList.toggle("show");
+// //Remove Menu
+// const removeMenu = () => navMenu.classList.remove("show");
+
+// //Toggle menu with hamburger
+// burgerIcon.addEventListener("click", function () {
+//   menuToggle();
+// });
+
+// //Remove menu by clicking outside navigation
+// mainElement.addEventListener("click", function () {
+//   removeMenu();
+// });
